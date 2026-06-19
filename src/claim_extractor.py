9c984eff7293ac030f56da149584524402daf8fc
Bikram Singh <133608313+bikramcodes@users.noreply.github.com>
2026-06-19 06:36:46 +0530

    Update claim_extractor.py

--- src/claim_extractor.py
+++ src/claim_extractor.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 import json
-import os
+import streamlit as st
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+openai_key = st.secrets["OPENAI_API_KEY"]
+
+client = OpenAI(api_key=openai_key)
 
 def extract_claims(text):
 
